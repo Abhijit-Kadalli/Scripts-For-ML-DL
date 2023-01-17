b8e892be68e7716c5952685eb42a797712100fe1
Abhijit Kadalli <abhijitkadalli@nitgoa.ac.in>
2023-01-17 11:29:18 +0530

    labelCounter for train and validation data

---
+++ labelCounter.py
@@ -0,0 +1,32 @@
+import glob
+import os
+
+labels_count_test =[]
+labels_count_val = []
+def get_label(label_path):
+    label_count =  []
+    
+    return label_count
+
+# Current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+current_dir = 'train'
+
+# Directory where the data will reside
+for label_path in glob.iglob(os.path.join(current_dir, "*.txt")):
+    with open(label_path, "r") as f:
+        for line in f:
+            line.split(" ")
+            labels_count_test[int(line[0])] += 1
+
+current_dir = 'val'
+for label_path in glob.iglob(os.path.join(current_dir, "*.txt")):
+    with open(label_path, "r") as f:
+        for line in f:
+            line.split(" ")
+            labels_count_val[int(line[0])] += 1
+
+# display the label count
+print(labels_count_test)
+print(labels_count_val)
